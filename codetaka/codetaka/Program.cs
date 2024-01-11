cb0792152bd30c1afc9d061a426774a97443766b
김치헌 <154493881+choneyKim@users.noreply.github.com>
2024-01-11 09:57:55 +0900

    Add files via upload

---
+++ codetaka/codetaka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+Solution sol = new Solution();
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+double result = sol.solution(numbers);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public double solution(int[] numbers)
+    {
+        double temp = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            temp += numbers[i];
+        }
+        double answer = temp / numbers.Length;
+        return answer;
+    }
+}
