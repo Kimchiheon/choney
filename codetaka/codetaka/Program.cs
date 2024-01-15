d5a9b86788e983e23ad64e9c9b4390acabfd630e
김치헌 <chk87a@gmail.com>
2024-01-15 10:05:48 +0900

    1/15 코드타카 추가

--- codetaka/codetaka/Program.cs
+++ codetaka/codetaka/Program.cs
@@ -32,3 +32,17 @@ public class Solution2
         return answer;
     }
 }
+
+public class Solution3
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0) answer += i;
+        }
+
+        return answer;
+    }
+}
