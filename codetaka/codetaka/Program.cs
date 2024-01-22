3bb4a9fbf84e13e612712caa6b9d64127173ada5
김치헌 <chk87a@gmail.com>
2024-01-22 10:04:27 +0900

    1/20코드타카

--- codetaka/codetaka/Program.cs
+++ codetaka/codetaka/Program.cs
@@ -1,110 +1,136 @@
 ﻿using System;
 
-Solution sol = new Solution();
-int[] numbers = { 1, 2, 3, 4, 5, 6 };
-double result = sol.solution(numbers);
-Console.WriteLine(result);
+
+//int[] numbers = { 1, 2, 3, 4, 5, 6 };
+//double result = sol.solution(numbers);
+//Console.WriteLine(result);
+
+//public class Solution
+//{
+//    public double solution(int[] numbers)
+//    {
+//        double temp = 0;
+//        for (int i = 0; i < numbers.Length; i++)
+//        {
+//            temp += numbers[i];
+//        }
+//        double answer = temp / numbers.Length;
+//        return answer;
+//    }
+//}
+
+//public class Solution2
+//{
+//    public int solution(int n)
+//    {
+//        int answer = 0;
+//        do
+//        {
+//            answer += n % 10;
+//            n /= 10;
+//        } while (n > 0);
+//        return answer;
+//    }
+//}
+
+//public class Solution3
+//{
+//    public int solution(int n)
+//    {
+//        int answer = 0;
+//        for (int i = 1; i <= n; i++)
+//        {
+//            if (n % i == 0) answer += i;
+//        }
+
+//        return answer;
+//    }
+//}
+
+
+//public class Solution4
+//{
+//    public int solution(int n)
+//    {
+//        int answer = 0;
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (n % (n - i) == 1)
+//            {
+//                answer = n - i;
+//            }
+
+//        }
+
+//        return answer;
+//    }
+//}
+
+//public class Solution5
+//{
+//    public long[] solution(int x, int n)
+//    {
+//        long[] answer = new long[n];
+//        for (long i = 0; i < n; i++)
+//        {
+//            answer[i] = x * (i + 1);
+//        }
+//        return answer;
+//    }
+//}
+
+//public class Solution6
+//{
+//    public int[] solution(long n)
+//    {
+//        int count = 0;
+//        long temp = 0;
+//        int[] answer = new int[] { };
+//        while (true)
+//        {
+//            answer[count] = (int)n % 10;
+//            temp = n / 10;
+//            count++;
+//            if (temp < 10) break;
+//        }
+//        return answer;
+//    }
+//}
+
+//public class Solution7
+//{
+//    public int solution(string s)
+//    {
+//        bool isNum = false;
+//        int answer = 0;
+//        isNum = int.TryParse(s, out answer);
+
+//        return answer;
+//    }
+//}
+
 
 public class Solution
 {
-    public double solution(int[] numbers)
+    public long solution(long n)
     {
-        double temp = 0;
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            temp += numbers[i];
-        }
-        double answer = temp / numbers.Length;
-        return answer;
-    }
-}
-
-public class Solution2
-{
-    public int solution(int n)
-    {
-        int answer = 0;
-        do
-        {
-            answer += n % 10;
-            n /= 10;
-        } while (n > 0);
-        return answer;
-    }
-}
-
-public class Solution3
-{
-    public int solution(int n)
-    {
-        int answer = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            if (n % i == 0) answer += i;
-        }
-
-        return answer;
-    }
-}
-
-
-public class Solution4
-{
-    public int solution(int n)
-    {
-        int answer = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (n % (n - i) == 1)
-            {
-                answer = n - i;
-            }
-
-        }
-
-        return answer;
-    }
-}
-
-public class Solution5
-{
-    public long[] solution(int x, int n)
-    {
-        long[] answer = new long[n];
-        for (long i = 0; i < n; i++)
-        {
-            answer[i] = x * (i + 1);
-        }
-        return answer;
-    }
-}
-
-public class Solution6
-{
-    public int[] solution(long n)
-    {
-        int count = 0;
-        long temp = 0;
-        int[] answer = new int[] { };
+        long x = (int)n / 2;
         while (true)
         {
-            answer[count] = (int)n % 10;
-            temp = n / 10;
-            count++;
-            if (temp < 10) break;
+            if (x * x == n) return (x + 1) * (x + 1);
+            if (x * x > n) x = (int)x / 2;
+            if (x * x < n)
+            {
+                x++;
+                if (x * x > n) return -1;
+
+
+            }
         }
-        return answer;
+
+
     }
+
 }
 
-public class Solution7
-{
-    public int solution(string s)
-    {
-        bool isNum = false;
-        int answer = 0;
-        isNum = int.TryParse(s, out answer);
 
-        return answer;
-    }
-}
--- codetaka/codetaka/obj/Debug/net8.0/codetaka.AssemblyInfo.cs
+++ codetaka/codetaka/obj/Debug/net8.0/codetaka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codetaka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f9fa9e027cc897958251983df45fc231025e61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c7aaf95441a3a63729c36351743363fa03dfdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("codetaka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codetaka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
