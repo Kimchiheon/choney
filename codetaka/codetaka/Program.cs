3f9eb5a53c36b398ba670a246fdb1d155fc8d86b
김치헌 <chk87a@gmail.com>
2024-01-26 09:16:47 +0900

    1/26 코드타카

--- codetaka/codetaka/Program.cs
+++ codetaka/codetaka/Program.cs
@@ -161,3 +161,29 @@ using System.Linq;
 //    }
 //}
 
+//public class Solution
+//{
+//    public long solution(int a, int b)
+//    {
+//        long answer = 0;
+//        if (a > b)
+//        {
+//            for (int i = b; i <= a; i++)
+//            {
+//                answer += i;
+//            }
+//        }
+//        else if (b > a)
+//        {
+//            for (int i = a; i <= b; i++)
+//            {
+//                answer += i;
+//            }
+//        }
+//        else answer = a;
+
+//        return answer;
+//    }
+//}
+
+
